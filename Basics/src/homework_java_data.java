ac6b81a160d47c29ba272d87d528b5e12aff77dd
NickWallace2001 <70587660+NickWallace2001@users.noreply.github.com>
2020-09-17 03:16:36 -0400

    homework

---
+++ Basics/src/homework_java_data.java
@@ -0,0 +1,15 @@
+
+public class homework_java_data {
+	public static void main(String[] args) {
+		
+		String name = "Nick";
+		System.out.println("I am " + name);
+		
+		int age = 19;
+		System.out.println("I am " + age + " years old");
+		
+		float shoeSize = 11.5f;
+		System.out.println("My shoe size is " + shoeSize);
+	}
+
+}
