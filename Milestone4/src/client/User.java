d9a8e6fe8b194fb309f1e79fd862507f08a9757f
NickWallace2001 <70587660+NickWallace2001@users.noreply.github.com>
2020-12-11 16:55:10 -0500

    Milestone4 is started and completed

---
+++ Milestone4/src/client/User.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JEditorPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class User extends JPanel {
+    private String name;
+    private JEditorPane nameField;
+
+    public User(String name) {
+	this.name = name;
+	nameField = new JEditorPane();
+	nameField.setContentType("text/html");
+	nameField.setText(name);
+	nameField.setEditable(false);
+	this.setLayout(new BorderLayout());
+	this.add(nameField);
+    }
+    
+    public void setName(String name) {
+	nameField.setText(name);
+    }
+
+    public String getName() {
+	return name;
+    }
+}
